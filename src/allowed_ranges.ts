7c9e0c479522bc0979af32df6505aa2b5dc45641
Đorđe Milanović <djordje200179@gmail.com>
2023-12-16 15:57:10 +0100

    More property ranges added

--- src/allowed_ranges.ts
+++ src/allowed_ranges.ts
@@ -5,11 +5,48 @@ export interface Range {
 
 export default {
 	"ph": {
-		min: 5,
-		max: 7,
+		min: 6.5,
+		max: 8.5
 	},
-	"temperature":{
+	"temperature": {},
+	"oxygen": {},
+	"orv": {
+		min: 300,
+		max: 500
+	},
+	"conductivity": {
 		min: 0,
-		max: 50,
+		max: 1000
 	},
+	"salinity": {
+		max: 600
+	},
+	"tds": {
+		min: 150,
+		max: 250
+	},
+	"turbidity": {
+		max: 1
+	},
+	"tss": {
+		max: 30
+	},
+	"ammonia": {
+		max: 0.5
+	},
+	"nitrate": {
+		max: 10
+	},
+	"chloride": {
+		max: 250
+	},
+	"sodium": {
+		max: 60
+	},
+	"calcium": {
+		max: 300
+	},
+	"bga": {
+		max: 30
+	}
 } as { [name: string]: Range };
