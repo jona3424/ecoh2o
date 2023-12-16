4198f82eafa98ef6b3d23535381328ce98814735
dsendelj7 <dsendelj7@gmail.com>
2023-12-16 14:34:34 +0100

    Dodao prvu verziju mape sa stilom

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -6,5 +6,88 @@ import { Component } from '@angular/core';
   styleUrls: ['./mapa.component.scss']
 })
 export class MapaComponent {
-
+  mapOptions: google.maps.MapOptions = {
+    center: { lat: 44.7988288, lng: 20.3674271 },
+    zoom : 14,
+    styles: [
+      {
+          "featureType": "administrative",
+          "elementType": "labels.text.fill",
+          "stylers": [
+              {
+                  "color": "#444444"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "color": "#f2f2f2"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "lightness": 45
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "labels.icon",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "transit",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "color": "#46bcec"
+              },
+              {
+                  "visibility": "on"
+              }
+          ]
+      }
+  ]
+ }
 }
