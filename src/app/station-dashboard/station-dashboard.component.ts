f3e4c0d7a504fd4d74f2a13be1720fef3ba5c468
Nikola Petric <46651607+jona3424@users.noreply.github.com>
2023-12-16 21:16:23 +0100

    Dodao working dio za chartove i reports

--- src/app/station-dashboard/station-dashboard.component.ts
+++ src/app/station-dashboard/station-dashboard.component.ts
@@ -3,13 +3,21 @@ import { ActivatedRoute } from "@angular/router";
 import Station from "../models/station";
 import { DatabaseService } from "../services/database.service";
 import Measurement from "../models/measurement";
-
 @Component({
 	selector: 'app-station-dashboard',
 	templateUrl: './station-dashboard.component.html',
 	styleUrls: ['./station-dashboard.component.scss']
 })
 export class StationDashboardComponent implements OnInit {
+
+
+
+	public datasets: any;
+	public data: any;
+	public clicked: boolean = true;
+	public clicked1: boolean = false;
+
+
 	private readonly stationId: string;
 	public station?: Station;
 	public measurements?: Measurement[]
@@ -19,6 +27,35 @@ export class StationDashboardComponent implements OnInit {
 	}
 
 	public ngOnInit() {
+
+		this.datasets = [
+      [0, 20, 10, 30, 15, 40, 20, 60, 60],
+      [0, 20, 5, 25, 10, 30, 15, 40, 40]
+    ];
+    this.data = this.datasets[0];
+
+
+    var chartOrders = document.getElementById('chart-orders');
+
+    // parseOptions(Chart, chartOptions());
+
+
+    // var ordersChart = new Chart(chartOrders, {
+    //   type: 'bar',
+    //   options: chartExample2.options,
+    //   data: chartExample2.data
+    // });
+
+    // var chartSales = document.getElementById('chart-sales');
+
+    // this.salesChart = new Chart(chartSales, {
+	// 		type: 'line',
+	// 		options: chartExample1.options,
+	// 		data: chartExample1.data
+	// 	});
+
+
+
 		this.database.getStation(this.stationId).then(station => {
 			if (!station)
 				return;
@@ -31,4 +68,8 @@ export class StationDashboardComponent implements OnInit {
 			});
 		});
 	}
+	public updateOptions() {
+		// this.salesChart.data.datasets[0].data = this.data;
+		// this.salesChart.update();
+	  }
 }
